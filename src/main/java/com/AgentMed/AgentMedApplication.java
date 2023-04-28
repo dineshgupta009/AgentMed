db914b32d64122527d3b78d3e242bf282be0e0fb
Dinesh Gupta <Dineshg000@gmail.com>
2023-04-28 17:49:00 +0530

    Initial commit

---
+++ src/main/java/com/AgentMed/AgentMedApplication.java
@@ -0,0 +1,13 @@
+package com.AgentMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AgentMed/AgentMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgentMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
